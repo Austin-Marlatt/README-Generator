79e2d0f65fa6c6144b0c3002211ab2858091277f
Austin Marlatt <austin.marlatt1@gmail.com>
2024-04-14 17:52:00 -0700

    Added finishing touches to the markdown generator

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,76 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
+function renderLicenseLink(license) {
+  return `* [License](#license)`;
+};
+
+
+function renderLicenseSection(userInput) {
+  return ` ## License
+  
+  This project is licenseed under the ${license} license`
+}
+
+
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
 
+## Description 
+
+${data.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+
+*[Usage](#usage)
+
+${renderLicenseLink(data.license)}
+
+*[Contributing](#contributing)
+
+*[Tests](#tests)
+
+*[Questions](#questions)
+
+## Installation
+
+Run the following command to download the required dependencies: 
+
+\`\`\`
+${data.installation}
+\`\`\`
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+The following command will run tests:
+
+\`\`\`
+${data.tests}
+\`\`\`
+
+## Questions
+
+Have questions?
+
+Feel free to email me at ${data.email}
+
+Check out the other projects I have worked on at [${data.github}](https://github.com/${data.github}/)
+
+${renderLicenseSection(data.license)}
 `;
 }
 
